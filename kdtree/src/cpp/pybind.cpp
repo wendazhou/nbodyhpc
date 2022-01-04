a5bfba64b935ada0dbbb3ca98faf5a5ba0574121
Wenda Zhou <wz2247@nyu.edu>
2022-01-04 13:11:13 -0500

    basic pybind module build

--- kdtree/src/cpp/pybind.cpp
+++ kdtree/src/cpp/pybind.cpp
@@ -1,7 +1,154 @@
+#include <optional>
+
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "kdtree.hpp"
+#include <thread_pool.hpp>
 
 namespace py = pybind11;
 
+namespace {
+
+std::vector<wenda::kdtree::PositionAndIndex>
+make_positions_and_indices(py::array_t<float> const &points) {
+    if (points.ndim() != 2 || points.shape(1) != 3) {
+        throw std::runtime_error("positions must be a 2D array of shape (N, 3)");
+    }
+
+    auto points_view = points.unchecked<2>();
+
+    std::vector<wenda::kdtree::PositionAndIndex> positions(points.shape(0));
+
+    for (size_t i = 0; i < points.shape(0); ++i) {
+        positions[i].position[0] = points_view(i, 0);
+        positions[i].position[1] = points_view(i, 1);
+        positions[i].position[2] = points_view(i, 2);
+        positions[i].index = i;
+    }
+
+    return positions;
+}
+
+} // namespace
+
+namespace wenda {
+namespace kdtree {
+
+//! KDTree wrapper for pybind compatibility
+class PyKDTree : public wenda::kdtree::KDTree {
+    bool periodic_;
+    float box_size_;
+
+  public:
+    PyKDTree(PyKDTree const &) = default;
+    PyKDTree(PyKDTree &&) = default;
+
+    PyKDTree(
+        py::array_t<float> points, int leaf_size, int max_threads, std::optional<float> box_size)
+        : KDTree(
+              make_positions_and_indices(points),
+              {.leaf_size = leaf_size, .max_threads = max_threads}),
+          periodic_(box_size.has_value()), box_size_(box_size.value_or(1.0f)) {}
+
+    static PyKDTree from_points(
+        py::array_t<float> const &points, int leaf_size, int max_threads,
+        std::optional<float> box_size) {
+        py::gil_scoped_release nogil;
+        return PyKDTree(points, leaf_size, max_threads, box_size);
+    }
+
+    std::pair<py::array_t<float>, py::array_t<uint32_t>>
+    query(py::array_t<float> points, int k, int workers) {
+        if (k <= 0) {
+            throw std::runtime_error("k must be positive integer");
+        }
+
+        if (points.ndim() != 2 || points.shape(1) != 3) {
+            throw std::runtime_error("positions must be a 2D array of shape (N, 3)");
+        }
+
+        wenda::thread_pool pool(workers > 0 ? workers : std::thread::hardware_concurrency());
+
+        auto num_points = points.shape(0);
+        auto points_view = points.unchecked<2>();
+
+        float *result_distances = new float[num_points * k];
+        uint32_t *result_indices = new uint32_t[num_points * k];
+
+        if (periodic_) {
+            py::gil_scoped_release nogil;
+            auto distance_periodic = wenda::kdtree::L2PeriodicDistance<float>{box_size_};
+
+            pool.parallelize_loop(0, num_points, [&](size_t start, size_t end) {
+                for (size_t i = start; i < end; ++i) {
+                    auto result = find_closest(
+                        {points_view(i, 0), points_view(i, 1), points_view(i, 2)},
+                        k,
+                        distance_periodic);
+
+                    std::transform(
+                        result.begin(), result.end(), result_distances + i * k, [](auto const &r) {
+                            return r.first;
+                        });
+                    std::transform(
+                        result.begin(), result.end(), result_indices + i * k, [](auto const &r) {
+                            return r.second;
+                        });
+                }
+            });
+        } else {
+            py::gil_scoped_release nogil;
+            pool.parallelize_loop(0, num_points, [&](size_t start, size_t end) {
+                for (size_t i = start; i < end; ++i) {
+                    auto result = find_closest(
+                        {points_view(i, 0), points_view(i, 1), points_view(i, 2)},
+                        k,
+                        wenda::kdtree::L2Distance{});
+
+                    std::transform(
+                        result.begin(), result.end(), result_distances + i * k, [](auto const &r) {
+                            return r.first;
+                        });
+                    std::transform(
+                        result.begin(), result.end(), result_indices + i * k, [](auto const &r) {
+                            return r.second;
+                        });
+                }
+            });
+        }
+
+        py::capsule free_result_distances(
+            result_distances, [](void *ptr) { delete[] static_cast<float *>(ptr); });
+        py::capsule free_result_indices(
+            result_indices, [](void *ptr) { delete[] static_cast<uint32_t *>(ptr); });
+
+        py::array::ShapeContainer shape{(py::ssize_t)num_points, (py::ssize_t)k};
+
+        return {
+            py::array_t<float>{
+                shape, {k * sizeof(float), sizeof(float)}, result_distances, free_result_distances},
+            py::array_t<uint32_t>{
+                shape,
+                {k * sizeof(uint32_t), sizeof(uint32_t)},
+                result_indices,
+                free_result_indices}};
+    }
+};
+
+} // namespace kdtree
+
+} // namespace wenda
+
 PYBIND11_MODULE(_impl, m) {
     m.doc() = "Fast KD-tree for spatial data, including periodic boundary conditions.";
+
+    py::class_<wenda::kdtree::PyKDTree>(m, "KDTree")
+        .def(
+            py::init(&wenda::kdtree::PyKDTree::from_points),
+            py::arg("points"),
+            py::arg("leafsize") = 64,
+            py::arg("max_threads") = -1,
+            py::arg("boxsize") = std::nullopt);
 }
